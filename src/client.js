ede19187978351a964b296237860d7d4e956a429
hangingpulse <76062791+hangingpulse@users.noreply.github.com>
2021-04-13 14:55:38 +0200

    Updates

--- src/client.js
+++
@@ -1,9 +0,0 @@
-import * as contentful from 'contentful'
-
-const client = contentful.createClient({
-        space: process.env.REACT_APP_SPACE_ID,
-        accessToken: process.env.REACT_APP_ACCESS_TOKEN
-    })
-
-
-export default client
